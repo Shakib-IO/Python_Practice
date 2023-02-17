4ff2d9a73b2c9ba429c5d7a0349683c285062ab8
Shakib Khan <65369990+Shakib-IO@users.noreply.github.com>
2023-02-17 18:22:58 -0500

    Update Exercise.py

--- Practice/Exercise.py
+++ Practice/Exercise.py
@@ -10,3 +10,13 @@ def rm_s(str: s, num:int):
     new_s = s[num:]
     return new_s
 print(rm_s(s, 2))        
+
+# Exercise 5: Check if the first and last number of a list is the same
+
+x = [10, 20, 30, 40, 100]
+
+def check_is_same(x:list):
+    if x is None:
+        return None
+    return True if x[0]==x[-1] else False
+print(check_is_same(x))
