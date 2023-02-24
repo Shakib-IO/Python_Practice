33f10b2b091c8ac9d2dc0fadea7ac36de180cd98
Shakib Khan <65369990+Shakib-IO@users.noreply.github.com>
2023-02-24 14:24:20 -0500

    Update Exercise.py

--- Practice/Exercise.py
+++ Practice/Exercise.py
@@ -239,3 +239,23 @@ for i in range(l):
         list3.append(list2[i])
 
 print(list3)
+"""
+Exercise 5: Iterate both lists simultaneously
+Given a two Python list. Write a program to iterate both lists simultaneously and 
+display items from list1 in original order and items from list2 in reverse order.
+
+Given
+list1 = [10, 20, 30, 40]
+list2 = [100, 200, 300, 400]
+
+Expected output:
+10 400
+20 300
+30 200
+40 100
+"""
+list1 = [10, 20, 30, 40]
+list2 = [100, 200, 300, 400]
+
+for i, j in zip(list1, list2[::-1]):
+    print(i, j)
