35656a4c34772c821f5f937a67709a867e88194b
Shakib Khan <65369990+Shakib-IO@users.noreply.github.com>
2023-03-03 17:10:33 -0500

    Update OOP.py

--- Practice/OOP.py
+++ Practice/OOP.py
@@ -1,1 +1,12 @@
+"""
+OOP Exercise 1: Create a Class with instance attributes
+Write a Python program to create a Vehicle class with max_speed and mileage 
+instance attributes
+"""
+class Vehicle:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
 
+car1 = Vehicle(100, 10)
+print(car1.max_speed, car1.mileage)
