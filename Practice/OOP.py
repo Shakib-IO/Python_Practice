7293c6249885974d6f28cf5c6fa448d2291237b0
Shakib Khan <65369990+Shakib-IO@users.noreply.github.com>
2023-03-03 17:39:34 -0500

    Update OOP.py

--- Practice/OOP.py
+++ Practice/OOP.py
@@ -10,3 +10,27 @@ class Vehicle:
 
 car1 = Vehicle(100, 10)
 print(car1.max_speed, car1.mileage)
+
+"""
+OOP Exercise 4: Class Inheritance
+Given:
+Create a Bus class that inherits from the Vehicle class. Give the capacity 
+argument of Bus.seating_capacity() a default value of 50.
+"""
+class Vehicle:
+    def __init__(self, name , max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+    
+    def seating_capacity(self, capacity):
+        return f"The Seating capacity of a {self.name} is {capacity}"
+class Bus(Vehicle):
+    # def __init__(self, name, max_speed, mileage):
+    #     super().__init__(name, max_speed, mileage)
+    def seating_capacity(self, capacity = 100):
+        return super().seating_capacity(capacity)
+
+B = Bus("School Bus", 100, 10)
+print(f"{B.seating_capacity()}")
+
