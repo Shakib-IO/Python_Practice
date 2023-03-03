f8f15e3730a6c8b03b0d0891aafb081a2f38cfa8
Shakib Khan <65369990+Shakib-IO@users.noreply.github.com>
2023-03-02 22:51:51 -0500

    Update Dictionary.py

--- Practice/Dictionary.py
+++ Practice/Dictionary.py
@@ -79,4 +79,11 @@ sample_dict = {
 }
 print(min(sample_dict, key=sample_dict.get))
 
-
+#### Tuple
+Exercise 2: Access value 20 from the tuple
+tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
+print(tuple1[1][1])
+    
+    
+    
+    
