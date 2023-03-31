73f2bccbc4954940df2c38ccf2ab1c7ead6ecd25
Shakib Khan <65369990+Shakib-IO@users.noreply.github.com>
2023-03-31 17:54:34 -0400

    Update Stack.py

--- Practice/Stack.py
+++ Practice/Stack.py
@@ -49,3 +49,64 @@ s.print_stack()
 ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
                                                                 Exercise
 """
+# Stack: Push for Stack That Uses List (⚡ Interview Question)
+class Stack:
+    def __init__(self):
+        self.stack_list = []
+        
+    def print_stack(self):
+        for i in range(len(self.stack_list)-1, -1, -1):
+            print(self.stack_list[i])
+            
+    def push(self, value):
+        self.stack_list.append(value)
+        
+my_stack = Stack()
+my_stack.push(1)
+my_stack.push(2)
+my_stack.push(3)
+my_stack.print_stack()
+
+# Stack: Pop for Stack That Uses List (⚡ Interview Question)
+class Stack:
+    def __init__(self):
+        self.stack_list = []
+
+    def print_stack(self):
+        for i in range(len(self.stack_list)-1, -1, -1):
+            print(self.stack_list[i])
+
+    def is_empty(self):
+        return len(self.stack_list) == 0
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack_list[-1]
+
+    def size(self):
+        return len(self.stack_list)
+
+    def push(self, value):
+        self.stack_list.append(value)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack_list.pop()
+            
+my_stack = Stack()
+my_stack.push(1)
+my_stack.push(2)
+my_stack.push(3)
+
+print("Stack before pop():")
+my_stack.print_stack()
+
+print("\nPopped node:")
+print(my_stack.pop())
+
+print("\nStack after pop():")
+my_stack.print_stack()
