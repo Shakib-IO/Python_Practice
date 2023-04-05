960d1fd4a69f29ad282f6a12558d729874d06ccb
Shakib Khan <65369990+Shakib-IO@users.noreply.github.com>
2023-04-05 17:42:01 -0400

    Update Trees.py

--- Practice/Trees.py
+++ Practice/Trees.py
@@ -56,3 +56,49 @@ class BinarySearchTree:
             if current_node.right is not None:
                 que.append(current_node.right)
         return res
+    
+    def dfs_pre_order(self):
+        res = []
+        def traverse(current_node):
+            res.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+        traverse(self.root)
+        return res
+    
+    def dfs_post_order(self):
+        res = []
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            res.append(current_node.value)
+        traverse(self.root)
+        return res
+    def dfs_in_order(self):
+        res = []
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            res.append(current_node.value)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            
+        traverse(self.root)
+        return res
+    
+my_tree = BinarySearchTree()
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
+print(my_tree.BFS())
+print(my_tree.dfs_pre_order())
+print(my_tree.dfs_post_order())
+print(my_tree.dfs_in_order())
