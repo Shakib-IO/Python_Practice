14c40e96e0570e250cd7ab52a4740bd610567870
Shakib Khan <65369990+Shakib-IO@users.noreply.github.com>
2024-01-25 13:49:15 -0500

    Update Composition is Better than Inheritance.py

--- Practice/Design Patterns/Composition is Better than Inheritance.py
+++ Practice/Design Patterns/Composition is Better than Inheritance.py
@@ -1,3 +1,4 @@
 # https://youtu.be/0mcP8ZpUR38
 # https://realpython.com/inheritance-composition-python/#:~:text=Composition%20enables%20you%20to%20reuse,one%20class%20from%20the%20other.
 # https://youtu.be/hxGOiiR9ZKg
+# https://github.com/ArjanCodes/2021-composition-vs-inheritance/tree/main
