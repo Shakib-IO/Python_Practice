1750ed10041f3196a5ab14b0b89c909af452115f
Shakib Khan <65369990+Shakib-IO@users.noreply.github.com>
2024-03-17 15:04:43 -0400

    Create Dependency_Injection.py

---
+++ Practice/Python_structural/Dependency_Injection.py
@@ -0,0 +1,9 @@
+# What is Dependency Injection?
+"""
+In simpler terms, it's about providing objects with the things they need to function, rather than making them create those things themselves.
+
+Here's a breakdown of the concept:
+Dependency: An object relies on another object (or service) to perform its tasks. This "other object" is considered a dependency.
+Injection: Instead of creating its dependencies internally, an object receives them from external code. This "external code" is often called an injector.
+
+"""
